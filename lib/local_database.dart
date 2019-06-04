19109f5dc5aa0f6f4e82408222da266f1c3b41b6
Blakexx <blake@platypus.land>
2019-06-04 00:42:15 -0500

    All

---
+++ lib/local_database.dart
@@ -0,0 +1,181 @@
+library local_database;
+
+import "dart:io";
+import "dart:convert";
+
+class Database{
+
+  Directory _base;
+
+  Database(String b){
+    _base = new Directory(b)..createSync(recursive: true);
+  }
+
+  String get path => _base.path;
+
+  String _delim = Platform.pathSeparator;
+
+  operator []=(String path, dynamic data){
+    path = _fixPath(path);
+    String s;
+    try{
+      s = json.encode(data);
+    }catch(e){
+      throw new Exception("Invalid data (not json encodable)");
+    }
+    remove(path);
+    if(data is List){
+      List l = data;
+      data = new Map<String,dynamic>();
+      l.asMap().forEach((i,d)=>data[i.toString()] = d);
+    }
+    if(data is Map){
+      data.forEach((k,v)=>this[path==_delim?path+k:"$path/$k"]=v);
+    }else{
+      if(data==null||data==""){
+        remove(path);
+      }else{
+        if(path==_delim){
+          throw new Exception("Invalid Path (cannot write to root)");
+        }
+        new File(_base.path+path)..createSync(recursive: true)..writeAsStringSync(s);
+      }
+    }
+  }
+
+  operator [](String path){
+    path = _fixPath(path);
+    dynamic f = new File(_base.path+path);
+    if(!f.existsSync()){
+      f = new Directory(_base.path+path);
+    }
+    if(!f.existsSync()){
+      return null;
+    }else{
+      if(f is Directory){
+        Map<String, dynamic> map = new Map<String, dynamic>();
+        List<dynamic> files = f.listSync(recursive: true);
+        files.forEach((d){
+          if(d is Directory){
+            return;
+          }
+          String path = d.path;
+          path = path.substring(f.path.length);
+          path = _fixPath(path);
+          if(Platform.isMacOS&&path.split(_delim).last==".DS_Store"){
+            return;
+          }
+          List<String> paths = path.split(_delim);
+          paths.removeAt(0);
+          String last = paths.removeLast();
+          Map<String, dynamic> temp = map;
+          paths.forEach((s){
+            if(temp[s]!=null){
+              temp = temp[s];
+            }else{
+              temp[s] = new Map<String,dynamic>();
+              temp = temp[s];
+            }
+          });
+          temp[last] = json.decode(d.readAsStringSync());
+        });
+        _convertAllLists(map, null, null);
+        if(_isList(map)){
+          return _mapToList(map);
+        }
+        return map;
+      }else{
+        String data = f.readAsStringSync();
+        return json.decode(data);
+      }
+    }
+  }
+
+  List<dynamic> _mapToList(Map<String, dynamic> map){
+    List<dynamic> list = new List<dynamic>();
+    list.length = map.length;
+    for(int i = 0; i<list.length;i++){
+      list[i] = map[i.toString()];
+    }
+    return list;
+  }
+
+  void _convertAllLists(Map map, dynamic parentMap, dynamic key){
+    if(_isList(map)){
+      if(parentMap!=null){
+        parentMap[key] = _mapToList(map);
+        List l = parentMap[key];
+        for(int i = 0; i<l.length;i++){
+          if(l[i] is Map){
+            _convertAllLists(l[i], parentMap[key], i);
+          }
+        }
+      }else{
+        List l = _mapToList(map);
+        for(int i = 0; i<map.keys.length;i++){
+          if(l[i] is Map){
+            _convertAllLists(map[i.toString()], map, i.toString());
+          }
+        }
+      }
+      return;
+    }
+    map.forEach((k,v){
+      if(v is Map){
+        _convertAllLists(v,map,k);
+      }
+    });
+  }
+
+  bool _isList(Map map){
+    dynamic keys = map.keys;
+    bool allInts = keys.every((s)=>new RegExp("\\d+").hasMatch(s));
+    bool sequential = true;
+    for(int i = 0; i<keys.length;i++){
+      if(!keys.contains("$i")){
+        sequential = false;
+        break;
+      }
+    }
+    return allInts&&sequential;
+  }
+
+  String _fixPath(String path){
+    path = path.replaceAll("/",_delim).replaceAll("\\", _delim);
+    if(path.length==0||path.substring(0,1)!=_delim){
+      path = _delim+path;
+    }
+    if(path.length>2&&path.endsWith(_delim)){
+      path = path.substring(0,path.length-1);
+    }
+    return path;
+  }
+
+  dynamic remove(String path){
+    path = _fixPath(path);
+    dynamic data = this[path];
+    dynamic f = new File(_base.path+path);
+    if(f.existsSync()){
+      Directory parent = f.parent;
+      f.deleteSync(recursive: true);
+      while(parent.path!=_base.path&&parent.listSync().length==0){
+        parent.deleteSync(recursive: true);
+        parent = parent.parent;
+      }
+    }else{
+      f = new Directory(_base.path+path);
+      if(f.existsSync()){
+        Directory parent = f.parent;
+        f.deleteSync(recursive: true);
+        while(parent.path!=_base.path&&parent.listSync().length==0){
+          parent.deleteSync(recursive: true);
+          parent = parent.parent;
+        }
+      }
+    }
+    if(path==_delim){
+      _base.createSync();
+    }
+    return data;
+  }
+}
